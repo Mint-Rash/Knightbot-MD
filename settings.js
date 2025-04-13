eb9c4ce77fe61faba40f0428f604dc0f1d023d4e
Mint-Rash <elonmusky359@gmail.com>
2025-04-13 21:02:30 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
-  author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  packname: 'Thompson',
+  author: 'Thompson',
+  botName: "Thompson Bot",
+  botOwner: 'Thompson', // Your name
+  ownerNumber: '', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
